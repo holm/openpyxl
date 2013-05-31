2f5593c815c96472528f303ff06c0993fdd5d3e9
Christian Holm <holm@podio.com>
2013-05-31 12:58:45 +0200

    This is the correct way to check for none

--- openpyxl/reader/workbook.py
+++ openpyxl/reader/workbook.py
@@ -29,7 +29,7 @@
 from openpyxl.shared.xmltools import fromstring, QName
 from openpyxl.shared.ooxml import NAMESPACES
 from openpyxl.workbook import DocumentProperties
-from openpyxl.shared.date_time import W3CDTF_to_datetime,CALENDAR_WINDOWS_1900,CALENDAR_MAC_1904
+from openpyxl.shared.date_time import W3CDTF_to_datetime, CALENDAR_WINDOWS_1900, CALENDAR_MAC_1904
 from openpyxl.namedrange import NamedRange, NamedRangeContainingValue, split_named_range, refers_to_range
 
 import datetime
@@ -77,9 +77,9 @@ def read_properties_core(xml_source):
 
 
 def read_excel_base_date(xml_source):
-    root = fromstring(text = xml_source)
+    root = fromstring(text=xml_source)
     wbPr = root.find(QName('http://schemas.openxmlformats.org/spreadsheetml/2006/main', 'workbookPr').text)
-    if (wbPr and 'date1904' in wbPr.keys() and wbPr.attrib['date1904'] in ('1', 'true')):
+    if (wbPr is not None and 'date1904' in wbPr.keys() and wbPr.attrib['date1904'] in ('1', 'true')):
         return CALENDAR_MAC_1904
 
     return CALENDAR_WINDOWS_1900
